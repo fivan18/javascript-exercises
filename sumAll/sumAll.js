097b2ed2feb9b786658330d5acc99f275e76e349
Ivan <fivanunam@gmail.com>
2019-11-04 13:00:12 -0600

    sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    if(
+        isNaN(args[0]) || isNaN(args[1]) || 
+        args[0] < 0 || args[1] < 0 ||
+        typeof args[0] === "string" || typeof args[1] === "string"
+        ){
+        return 'ERROR';
+    }
 
+    args.sort((a,b) => a-b);
+
+    let count = 0;
+    for(let i = args[0]; i <= args[1]; i++){
+        count += i;
+    }
+    return count;
+    
 }
 
 module.exports = sumAll
