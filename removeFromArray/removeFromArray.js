cb2dfaa5ed4c9cf2f61d6940a8f110c5742a9b8c
Ivan <fivanunam@gmail.com>
2019-11-04 12:31:28 -0600

    removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+// Using rest operator to manage indefinite number of parameters
+const removeFromArray = function(...args) {
+    let arr = args.shift();
+    let itemsToDelete = args;
+
+    return arr.filter(item => !itemsToDelete.includes(item));
 
 }
 
