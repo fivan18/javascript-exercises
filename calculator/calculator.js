a37c2864acfbd6000101cb6dfcb9b7cbc7352f3e
Ivan <fivanunam@gmail.com>
2019-11-09 11:45:49 -0600

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-function add () {
-	
+function add (a,b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.length === 0
+			? 0
+			: arr.reduce((accum, value) => accum += value, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.length === 0
+			? 0
+			: arr.reduce((accum, value) => accum *= value, 1);
 }
 
-function power() {
-	
+function power(base, power) {
+	return Math.pow(base,power);	
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num === 0){
+		return 1;
+	} else {
+		return num * factorial(num - 1);
+	}
 }
 
 module.exports = {
