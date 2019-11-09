24a7bf8194a17fa9074083ab7f1dd817340bf32e
Ivan <fivanunam@gmail.com>
2019-11-09 17:23:25 -0600

    findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
-
+function getAge(person){
+    let age;
+    if("yearOfDeath" in person){
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = (new Date().getFullYear()) - person.yearOfBirth;
+    }
+    return age;
 }
+
+let findTheOldest = function(people) {
+    return people.reduce((longest, person) => {
+        return getAge(person) > getAge(longest)
+                ? person
+                : longest;
+    }, people[0]);
+}
+
+
 
 module.exports = findTheOldest
