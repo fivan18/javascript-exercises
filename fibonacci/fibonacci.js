678fc69a47734aedc3a65154433d8a237eb3776a
Ivan <fivanunam@gmail.com>
2019-11-09 16:30:25 -0600

    fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let position = Number(num);
+
+    if(position < 1) return "OOPS";
+
+    let current = 1;
+    let last = 1;
+    while(position > 2){
+        let temp = current;
+        current = current + last;
+        last = temp;
+        position--;
+    }
+    return current;
 
 }
 
