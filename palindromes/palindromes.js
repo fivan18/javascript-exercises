1c53835e049e1add3d6763c3328690368a1be1ca
Ivan <fivanunam@gmail.com>
2019-11-09 12:07:57 -0600

    paindrome done

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    const wordClean = word.replace(/[^\w]/g, "").toLowerCase();
+    const reverseWord = wordClean.split("").reverse().join("");
 
+    return wordClean === reverseWord;
 }
 
 module.exports = palindromes
